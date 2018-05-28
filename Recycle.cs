5a64e88296a8b294998e9959d0cdfdd91f17fcad
Calytic <cyclonenetwork2015@gmail.com>
2018-05-27 20:48:19 -0400

    re-add test

--- Recycle.cs
+++ Recycle.cs
@@ -7,7 +7,7 @@ using System.Linq;
 
 namespace Oxide.Plugins
 {
-    [Info ("Recycle", "Calytic/rustservers.io", "2.1.4", ResourceId = 1296)]
+    [Info ("Recycle", "Calytic/rustservers.io", "2.1.5", ResourceId = 1296)]
     [Description ("Recycle crafted items to base resources")]
     class Recycle : RustPlugin
     {
